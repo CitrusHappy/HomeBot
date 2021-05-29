f8c858faeac4fd040c11eadea44ec965381859f0
Citrus <36286623+CitrusHappy@users.noreply.github.com>
2021-05-29 03:10:02 -0700

    add token to py

--- homebot.py
+++ homebot.py
@@ -11,7 +11,7 @@ PAT = 'EAAEY4Qf3fxABAAKpId8hWVgMZBQZAVQDMm3sZAM1czdjqaXyo06ZBrU81ISmBkWcbAUnBOxE
 @app.route('/', methods=['GET'])
 def handle_verification():
   print("Handling Verification.")
-  if request.args.get('hub.verify_token', '') == 'a':
+  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
     print("Verification successful!")
     return request.args.get('hub.challenge', '')
   else:
