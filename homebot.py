244a2e2c86108ce92a08abc6948faa94f55caa2f
Citrus <36286623+CitrusHappy@users.noreply.github.com>
2021-06-01 02:03:35 -0700

    rework script

--- homebot.py
+++ homebot.py
@@ -1,58 +1,40 @@
-from flask import Flask, request
-import json
+import credentials
 import requests
-
+from flask import Flask, request
 app = Flask(__name__)
 
-# This needs to be filled with the Page Access Token that will be provided
-# by the Facebook App that will be created.
-PAT = 'EAAEY4Qf3fxABAAKpId8hWVgMZBQZAVQDMm3sZAM1czdjqaXyo06ZBrU81ISmBkWcbAUnBOxE4JTkUFJs24LYaVtUOm52r3PMQXUvrUACQul5ZApogIO5EmtwxW9ePZADvgvy3wzXQSlf5fjQYAHUewJIZACl04WjD4mnOqvu8imKIlcfQRmFiok'
-
-@app.route('/', methods=['GET'])
-def handle_verification():
-  print("Handling Verification.")
-  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-    print("Verification successful!")
-    return request.args.get('hub.challenge', '')
-  else:
-    print("Verification failed!")
-    return 'Error, wrong validation token'
-
-@app.route('/', methods=['POST'])
-def handle_messages():
-  print ("Handling Messages")
-  payload = request.get_data()
-  print(payload)
-  for sender, message in messaging_events(payload):
-    print("Incoming from %s: %s") % (sender, message)
-    send_message(PAT, sender, message)
-  return "ok"
-
-def messaging_events(payload):
-  #Generate tuples of (sender_id, message_text) from the
-  #provided payload.
-
-  data = json.loads(payload)
-  messaging_events = data["entry"][0]["messaging"]
-  for event in messaging_events:
-    if "message" in event and "text" in event["message"]:
-      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-    else:
-      yield event["sender"]["id"], "I can't echo this"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 
-def send_message(token, recipient, text):
-  #Send the message text to recipient with id recipient.
 
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
-    }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print(r.text)
+# Adds support for GET requests to our webhook
+@app.route('/webhook',methods=['GET'])
+def webhook_authorization():
+    verify_token = request.args.get("hub.verify_token")
+    # Check if sent token is correct
+    if verify_token == credentials.WEBHOOK_VERIFY_TOKEN:
+        # Responds with the challenge token from the request
+        return request.args.get("hub.challenge")
+    return 'Unable to authorize.'
 
-if __name__ == '__main__':
-  app.run()
+
+@app.route("/webhook", methods=['POST'])
+def webhook_handle():
+    data = request.get_json()
+    message = data['entry'][0]['messaging'][0]['message']
+    sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+    if message['text']:
+        request_body = {
+                'recipient': {
+                    'id': sender_id
+                },
+                'message': {"text":"hello, world!"}
+            }
+        response = requests.post('https://graph.facebook.com/v5.0/me/messages?access_token='+credentials.TOKEN,json=request_body).json()
+        return response
+    return 'ok'
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
