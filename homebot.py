bf9b773e8ec95e36c5627e805319ff44bb75e2af
Citrus <36286623+CitrusHappy@users.noreply.github.com>
2021-08-11 19:00:22 -0700

    temporary test build

--- homebot.py
+++ homebot.py
@@ -15,48 +15,48 @@ EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 
 app = Flask(__name__)
-
-@app.route('/favicon.ico')
-def favicon():
-    return app.send_static_file('favicon.ico')
-
-
-@app.route('/')
-def hello_world():
-    return 'This is the homebot default page.'
-
-
-#single one-on-one chat response protocol
-#verify token to make sure its a message from facebook
-@app.route('/webhook', methods=['GET'])
-def webhook_authorization():
-    verify_token = request.args.get("hub.verify_token")
-    if verify_token == WEBHOOK_VERIFY_TOKEN and verify_token != None:
-        return request.args.get("hub.challenge")
-    return 'Failed authorization.'
-
-#send message back
-@app.route('/webhook', methods=['POST'])
-def webhook_handle():
-    data = request.get_json()
-    message = data['entry'][0]['messaging'][0]['message']
-    if message != None:
-        sender_id = data['entry'][0]['messaging'][0]['sender']['id']
-        if message.get('message', {}).get('text'):
-            message_text = message['text']
-            ints = chatbot.predict_class(message_text)
-            res = chatbot.get_response(ints, chatbot.intents)
-
-            request_body = {
-                    'recipient': {
-                        'id': sender_id
-                    },
-                    'message': {"text":res}
-                }
-            response = requests.post('https://graph.facebook.com/v11.0/me/messages?access_token='+ACCESS_TOKEN,json=request_body).json()
-            return response
-        return 'ok'
-    return 'empty message'
+#
+#@app.route('/favicon.ico')
+#def favicon():
+#    return app.send_static_file('favicon.ico')
+#
+#
+#@app.route('/')
+#def hello_world():
+#    return 'This is the homebot default page.'
+#
+#
+##single one-on-one chat response protocol
+##verify token to make sure its a message from facebook
+#@app.route('/webhook', methods=['GET'])
+#def webhook_authorization():
+#    verify_token = request.args.get("hub.verify_token")
+#    if verify_token == WEBHOOK_VERIFY_TOKEN and verify_token != None:
+#        return request.args.get("hub.challenge")
+#    return 'Failed authorization.'
+#
+##send message back
+#@app.route('/webhook', methods=['POST'])
+#def webhook_handle():
+#    data = request.get_json()
+#    message = data['entry'][0]['messaging'][0]['message']
+#    if message != None:
+#        sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+#        if message.get('message', {}).get('text'):
+#            message_text = message['text']
+#            ints = chatbot.predict_class(message_text)
+#            res = chatbot.get_response(ints, chatbot.intents)
+#
+#            request_body = {
+#                    'recipient': {
+#                        'id': sender_id
+#                    },
+#                    'message': {"text":res}
+#                }
+#            response = requests.post('https://graph.facebook.com/v11.0/me/messages?access_token='+ACCESS_TOKEN,json=request_body).json()
+#            return response
+#        return 'ok'
+#    return 'empty message'
 
 
 
