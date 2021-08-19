04aa410d4f0b9eaee105fcfb824c341e72e58a6c
Citrus <36286623+CitrusHappy@users.noreply.github.com>
2021-08-18 20:33:01 -0700

    Update database.py

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ import os
 DATABASE_URL = os.environ['DATABASE_URL']
 
 global cursor
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
 
 def create_table():
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS tbl_user (UserID int);")
     print('table created...')
